aa447f19f04b149394a710b967405079469d9dc9
Elias <devoeelias@gmail.com>
2018-10-16 16:11:15 -0500

    Add kaggle dataset data preparation

---
+++ parse_kaggle_headlines.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+import gensim, nltk
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
+from gensim import corpora, models
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import *
+
+np.random.seed(42)
+
+data = pd.read_csv('data/abcnews-date-text.csv', error_bad_lines=False);
+data_text = data[['headline_text']]
+data_text['index'] = data_text.index
+documents = data_text
+
+stemmer = SnowballStemmer("english")
+
+nltk.download('wordnet')
+
+def lemmatize_stemming(text):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
+
+
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
+            result.append(lemmatize_stemming(token))
+    return result
+
+processed_docs = documents['headline_text'].map(preprocess)
+
+dictionary = gensim.corpora.Dictionary(processed_docs)
+dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+
+tfidf = models.TfidfModel(bow_corpus)
+corpus_tfidf = tfidf[bow_corpus]
