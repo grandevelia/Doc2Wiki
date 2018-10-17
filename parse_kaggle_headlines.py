f12b5fd68ea53cbe0d14c8c0facec2bff4461051
Elias <devoeelias@gmail.com>
2018-10-17 10:29:22 -0500

    furhter separation

--- parse_kaggle_headlines.py
+++ parse_kaggle_headlines.py
@@ -2,12 +2,10 @@ import pandas as pd
 import numpy as np
 
 import gensim, nltk
-from gensim.utils import simple_preprocess
-from gensim.parsing.preprocessing import STOPWORDS
 from gensim import corpora, models
-from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import *
 
+from nlp_utils import lemmatize_stemming, preprocess
 
 np.random.seed(42)
 
@@ -16,20 +14,7 @@ data_text = data[['headline_text']]
 data_text['index'] = data_text.index
 documents = data_text
 
-stemmer = SnowballStemmer("english")
-
 nltk.download('wordnet')
-
-def lemmatize_stemming(text):
-    return stemmer.stem(WordNetLemmatizer().lemmatize(text, pos='v'))
-
-
-def preprocess(text):
-    result = []
-    for token in gensim.utils.simple_preprocess(text):
-        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
-            result.append(lemmatize_stemming(token))
-    return result
 
 processed_docs = documents['headline_text'].map(preprocess)
 
